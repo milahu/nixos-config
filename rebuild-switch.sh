6ae1ba8439311845e95ef6a5fb1478cec253a969
Milan Hauth <milahu@gmail.com>
2021-11-19 09:41:41 +0100

    update

--- rebuild-switch.sh
+++ rebuild-switch.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$0")"
 
-sudo nixos-rebuild switch --flake .#$(hostname) # not needed?
+#cd "$(dirname "$0")"
+#sudo nixos-rebuild switch --flake .#$(hostname) # not needed?
+
+sudo nixos-rebuild switch --flake /etc/nixos#$(hostname) # not needed?
 
 #sudo nixos-rebuild switch
